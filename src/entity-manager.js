1922d85307cd6e6fdbd01df5934f8f2e810d44c1
Gunnarr07 <jeremyrt88@gmail.com>
2016-09-14 15:27:01 -0500

    create entity-manager for collision detection

---
+++ src/entity-manager.js
@@ -0,0 +1,29 @@
+module.exports = exports = EntityManager;
+
+fucnt
+EntityManager = function(width, height, cellSize){
+    this.width = width;
+    this.height = height;
+    this.cellSize = cellSize;
+    this.widthInCells = Math.ceil(width / cellSize);
+    this.numberOfCells = this.widthInCells * Math.ceil(height / cellSize);
+    this.cells = [];
+    for(var i = 0; i < this.numberOfCells; i++){
+        this.cells[i] = [];
+    }
+}
+
+EntityManager.prototype.addEntity = function(entity){
+    var left = Math.floor(entity.x / this.cellSize);
+    var right = Math.ceil((entity.x + entity.width) / this.cellSize);
+    var top = Math.floor(entity.y / this.cellSize);
+    var bottom = Math.ceil((entity.y + entity.height) / this.cellSize);
+
+    for(var x = left; x < right; x++){
+        for(var y = top; y < bottom; y++){
+            this.cells[y * this.widthInCells + x].push(entity);
+            if(!entity.cells) entity.cells = [];
+            entity.cells.push({x: x, y: y});
+        }
+    }
+}
